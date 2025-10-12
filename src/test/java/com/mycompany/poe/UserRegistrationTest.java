ed993a0d8fd025dac848d7d7ee6d11201b724ca1
Destiny Eseh <ST10488186@rcconnect.edu.za>
2025-10-12 15:55:53 +0000

    new info

--- src/test/java/com/mycompany/poe/UserRegistrationTest.java
+++ src/test/java/com/mycompany/poe/UserRegistrationTest.java
@@ -18,109 +18,108 @@ public class UserRegistrationTest {
     @Test
     void testValidUsername() {
         UserRegistration reg = new UserRegistration();
-        assertTrue(reg.userNameCheck("abc_"));
+        assertTrue(reg.userNameCheck("dest_"));
     }
 
     @Test
     void testInvalidUsername_NoUnderscore() {
         UserRegistration reg = new UserRegistration();
-        assertFalse(reg.userNameCheck("abcde"));
+        assertFalse(reg.userNameCheck("destiny"));
     }
 
     @Test
     void testInvalidUsername_TooLong() {
         UserRegistration reg = new UserRegistration();
-        assertFalse(reg.userNameCheck("abc_de"));
+        assertFalse(reg.userNameCheck("desti_"));
     }
 
-    
     @Test
     void testValidPassword() {
         UserRegistration reg = new UserRegistration();
-        assertTrue(reg.passwordCheck("Abcdef1!"));
+        assertTrue(reg.passwordCheck("Destiny1!"));
     }
 
     @Test
     void testInvalidPassword_NoCapital() {
         UserRegistration reg = new UserRegistration();
-        assertFalse(reg.passwordCheck("abcdef1!"));
+        assertFalse(reg.passwordCheck("destiny1!"));
     }
 
     @Test
     void testInvalidPassword_NoDigit() {
         UserRegistration reg = new UserRegistration();
-        assertFalse(reg.passwordCheck("Abcdefg!"));
+        assertFalse(reg.passwordCheck("Destiny!"));
     }
 
     @Test
     void testInvalidPassword_NoSpecialChar() {
         UserRegistration reg = new UserRegistration();
-        assertFalse(reg.passwordCheck("Abcdef12"));
+        assertFalse(reg.passwordCheck("Destiny12"));
     }
 
     @Test
     void testInvalidPassword_TooShort() {
         UserRegistration reg = new UserRegistration();
-        assertFalse(reg.passwordCheck("Ab1!"));
+        assertFalse(reg.passwordCheck("Des1!"));
     }
 
     @Test
     void testValidPhone() {
         UserRegistration reg = new UserRegistration();
-        assertTrue(reg.phoneCheck("+27812345678"));
+        assertTrue(reg.phoneCheck("+27834567890"));
     }
 
     @Test
     void testInvalidPhone_WrongPrefix() {
         UserRegistration reg = new UserRegistration();
-        assertFalse(reg.phoneCheck("0812345678"));
+        assertFalse(reg.phoneCheck("0834567890"));
     }
 
     @Test
     void testInvalidPhone_WrongLength() {
         UserRegistration reg = new UserRegistration();
-        assertFalse(reg.phoneCheck("+2781234567"));
+        assertFalse(reg.phoneCheck("+2783456789"));
     }
 
     @Test
     void testRegisterUser_Success() {
         UserRegistration reg = new UserRegistration();
-        String result = reg.registerUser("abc_", "Abcdef1!", "+27812345678");
+        String result = reg.registerUser("dest_", "Destiny1!", "+27834567890");
         assertEquals("User registered successfully!", result);
     }
 
     @Test
     void testRegisterUser_InvalidUsername() {
         UserRegistration reg = new UserRegistration();
-        String result = reg.registerUser("abcdef", "Abcdef1!", "+27812345678");
+        String result = reg.registerUser("destiny", "Destiny1!", "+27834567890");
         assertTrue(result.contains("Username is not correctly formatted"));
     }
 
     @Test
     void testRegisterUser_InvalidPassword() {
         UserRegistration reg = new UserRegistration();
-        String result = reg.registerUser("abc_", "abcdefg", "+27812345678");
+        String result = reg.registerUser("dest_", "destiny", "+27834567890");
         assertTrue(result.contains("Password is not correctly formatted"));
     }
 
     @Test
     void testRegisterUser_InvalidPhone() {
         UserRegistration reg = new UserRegistration();
-        String result = reg.registerUser("abc_", "Abcdef1!", "0812345678");
+        String result = reg.registerUser("dest_", "Destiny1!", "0834567890");
         assertTrue(result.contains("Cell phone number is not valid"));
     }
 
     @Test
     void testLoginUser_Success() {
         UserRegistration reg = new UserRegistration();
-        reg.registerUser("abc_", "Abcdef1!", "+27812345678");
-        assertTrue(reg.loginUser("abc_", "Abcdef1!"));
+        reg.registerUser("dest_", "Destiny1!", "+27834567890");
+        assertTrue(reg.loginUser("dest_", "Destiny1!"));
     }
 
     @Test
     void testLoginUser_Failure() {
         UserRegistration reg = new UserRegistration();
-        reg.registerUser("abc_", "Abcdef1!", "+27812345678");
-        assertFalse(reg.loginUser("abc_", "WrongPass"));
+        reg.registerUser("dest_", "Destiny1!", "+27834567890");
+        assertFalse(reg.loginUser("dest_", "WrongPass!"));
     }
 }
