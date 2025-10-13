a670ce05f08af7347a5c870b3b38a8db30390a96
Destiny Eseh <ST10488186@rcconnect.edu.za>
2025-10-13 09:44:51 +0000

    Added Message section

--- src/main/java/com/mycompany/poe/POE.java
+++ src/main/java/com/mycompany/poe/POE.java
@@ -13,7 +13,7 @@ import javax.swing.JOptionPane;
 
 public class POE {
     public static void main(String[] args) {
-        
+
         UserRegistration reg = new UserRegistration();
 
         String u = JOptionPane.showInputDialog("Enter username: ");
@@ -30,11 +30,52 @@ public class POE {
         String lu = JOptionPane.showInputDialog("Enter username to login:");
         String lp = JOptionPane.showInputDialog("Enter password to login:");
 
-        if (reg.loginUser(lu, lp)) {
-            JOptionPane.showMessageDialog(null, "Login successful!\nWelcome " + lu + "!");
-        } else {
+        if (!reg.loginUser(lu, lp)) {
             JOptionPane.showMessageDialog(null, "Login failed. Incorrect username or password.");
+            return;
         }
+
+        JOptionPane.showMessageDialog(null, "Login successful!\nWelcome " + lu + "!");
+
+        int totalMessages = 0;
+        boolean keepSending = true;
+
+        while (keepSending) {
+            String messageID = JOptionPane.showInputDialog("Enter Message ID (max 10 characters):");
+            String recipient = JOptionPane.showInputDialog("Enter Recipient Cell (starts with 0, max 10 digits):");
+            String messageText = JOptionPane.showInputDialog("Enter your message (max 250 characters):");
+
+            if (messageText.length() > 250) {
+                JOptionPane.showMessageDialog(null, "Message too long! Please keep under 250 characters.");
+                continue;
+            }
+
+            Message message = new Message(messageID, recipient, messageText);
+
+            if (!message.checkMessageID()) {
+                JOptionPane.showMessageDialog(null, "Message ID is too long!");
+                continue;
+            }
+
+            if (message.checkRecipientCell() == 0) {
+                JOptionPane.showMessageDialog(null, "Recipient number invalid!");
+                continue;
+            }
+
+            String result = message.SentMessage();
+            if (result.equals("Message Sent")) {
+                totalMessages++;
+                JOptionPane.showMessageDialog(null, "Message sent successfully!\n\n" + message.getMessageDetails());
+            }
+
+            int choice = JOptionPane.showConfirmDialog(null,
+                    "Do you want to send another message?", "Continue", JOptionPane.YES_NO_OPTION);
+            if (choice != JOptionPane.YES_OPTION) {
+                keepSending = false;
+            }
+        }
+
+        JOptionPane.showMessageDialog(null, "You sent a total of " + totalMessages + " message(s).");
         
     }// end of main method
     
